255f5e011bfa1dca268c6083cf6cf823349e2c69
BlackBeard <moisesnt2@gmail.com>
2020-06-22 15:25:14 +0200

    changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
 
 
-app.get('/',(req,res) => res.send({name: "Moises WK"}))
+app.get('/',(req,res) => res.render('home'))
 
 
 // Handle the errors, without the route identification ==> '/'
