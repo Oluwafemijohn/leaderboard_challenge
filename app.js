4278fafea7fbff1f51c6221c5c5f444ae1be3908
BlackBeard <moisesnt2@gmail.com>
2020-06-23 07:29:33 +0200

    completed cicle

--- app.js
+++ app.js
@@ -3,21 +3,69 @@ const path = require('path')
 const express = require('express')
 const PORT = process.env.PORT || 4000
 const app =  express()
-
+const csv = require('csvtojson')
+const fs = require('fs')
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'assets')))
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
 
+let dirPath = path.resolve(__dirname,'assets/file')
+let filecsv = null, filejson = null
 
 app.get('/',(req,res) => res.render('home'))
+app.get('/csv',(req,res) => {
+    fs.readdir(dirPath, (err,files)=> {
+        if(err) res.send({ error: true, message: 'There is no csv file or incorrect path...'})
+        files.forEach(file => {
+            if(path.extname(file).toLowerCase() === '.csv')  filecsv = file
+        })
+        if(filecsv !== null ){
+            csv().fromFile(path.join(dirPath,filecsv))
+            .then(csv => {
+                csv.sort((a, b) => parseFloat(b["TOTAL POINTS"]) - parseFloat(a["TOTAL POINTS"]) );
+                res.send(Object.assign({ error: false }, {data : csv} ))
+            })
+        } else res.send({ error: true, message: 'There is no csv file...'})
+    })
+})
+
+fs.readdir(dirPath, (err,files)=> {
+    files.forEach(file => {
+        if(path.extname(file).toLowerCase() === '.csv')  filecsv = file
+    })
+    if(filecsv !== null ){
+        csv().fromFile(path.join(dirPath,filecsv))
+        .then(csv => {
+            let data = JSON.stringify(csv);
+            const filename = filecsv.split('.').slice(0, -1).join('.')
+            fs.writeFileSync(path.join(dirPath,filename+".json"), data);
+        })
+    }
+})
+
+app.get('/json',(req,res) => {
+    fs.readdir(dirPath, (err,files)=> {
+        if(err) res.send({ error: true, message: 'There is no csv file or incorrect path...'})
+        files.forEach(file => {
+            if(path.extname(file).toLowerCase() === '.json')  filejson = file
+        });
+        if (filejson !== null) {
+            let rawdata = fs.readFileSync(path.join(dirPath,filejson))
+            let jsondata = JSON.parse(rawdata.toString())
+            jsondata.sort((a, b) => parseFloat(b["TOTAL POINTS"]) - parseFloat(a["TOTAL POINTS"]) );
+
+            res.send(Object.assign({ error: false }, { data: jsondata } ))
+        } else res.send({ error: true, message: 'There is no csv file...'})
+    })
+})
 
 
 // Handle the errors, without the route identification ==> '/'
-// app.use((req, res, next) => {
-//     res.status(404).render('pages/errorHandler', { routError: 'Sorry, this route cannot be accessed...' })
-// })
+app.use((req, res, next) => {
+    res.status(404).render('errorHandler', { routError: 'Sorry, this route cannot be accessed...' })
+})
 
 // Set SERVER ON
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
---
+++ assets/js/main.js
@@ -0,0 +1,83 @@
+// API Caller Class
+class easyHTTP {
+    // This is in an asynchroous way by wrapping it into a Promise
+    async get(url) {
+        // Better way to get Data from API
+        const res = await fetch(url);
+        return await res.json();
+    }
+
+    async post(url, data) {
+        // Better way to get Data from API
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+        return await res.json();
+    }
+}
+
+const http = new easyHTTP();
+let currUrl = window.location.href;
+
+const getData = async (e) => {
+    e.preventDefault();
+    let cusUrl = currUrl += `json`;
+    let result = {};
+    await http.get(cusUrl)
+        .then(resps => result = resps)
+        .catch(err => console.log(err));
+
+    let th = '<th scope="col">#</th>'
+    let tr = ''
+    let count = 0
+    result.data.forEach((element) => {
+        count++
+        if (count <= 3) {
+            tr += `<tr class="bg-info">
+                <th scope="row">${count}</th>
+                <td >${element["FULL NAME"]}</td>
+                <td>${element["USERNAME"]}</td>
+                <td>${element["EMAIL"]}</td>
+                <td>${element["TOTAL POINTS"]}</td>
+                <td style="display: flex; align-items: center; justify-content: center; " >
+                <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" target="_blank" class="twitter-share-button pr-2" data-text="HNGi7 Central Leaderboard\n\nMY TOTAL POINTS: ${element["TOTAL POINTS"]}."  data-hashtags="HNGi7"  data-show-count="false"  data-url="\n\nFrom: ${currUrl}\n">Tweet</a>
+
+                <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fleaderbrd.herokuapp.com%2F&amp;src=sdkpreparse" class=" fb-share-button fb-xfbml-parse-ignore" style="margin-top: -4px; margin-left: 10px;" data-href="https://leaderbrd.herokuapp.com/" data-layout="button" data-size="small">Facebook</a>
+                </td>
+            </tr>`
+        } else {
+            tr += `<tr>
+                <th scope="row">${count}</th>
+                <td>${element["FULL NAME"]}</td>
+                <td>${element["USERNAME"]}</td>
+                <td>${element["EMAIL"]}</td>
+                <td>${element["TOTAL POINTS"]}</td>
+                <td style="display: flex; align-items: center; justify-content: center; " >
+                <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" target="_blank" class="twitter-share-button pr-2" data-text="HNGi7 Central Leaderboard\n\nMY TOTAL POINTS: ${element["TOTAL POINTS"]}."  data-hashtags="HNGi7"  data-show-count="false"  data-url="\n\nFrom: ${currUrl}\n">Tweet</a>
+
+                <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fleaderbrd.herokuapp.com%2F&amp;src=sdkpreparse" class=" fb-share-button fb-xfbml-parse-ignore" style="margin-top: -4px; margin-left: 10px;" data-href="https://leaderbrd.herokuapp.com/" data-layout="button" data-size="small">Facebook</a>
+                </td>
+            </tr>`
+        }
+        if (count === 1) {
+            for (const property in element) {
+                th += `<th scope="col">${property}</th>`
+            }
+        }
+    });
+
+    th += '<th scope="col" style="width: 90px;">Share</th>'
+
+    document.querySelector('#total').innerHTML = await `Total: ${count}`
+    const thead = document.querySelector('#thead')
+    thead.innerHTML = await th
+
+    const tbody = document.querySelector('#tbody')
+    tbody.innerHTML = await tr
+    
+}
+
+// Get Posts on DOM Load
+document.addEventListener('DOMContentLoaded', getData);
